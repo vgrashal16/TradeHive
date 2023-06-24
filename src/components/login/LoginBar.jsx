85179807c4e7a3b71e5be8b4340386beaa62e8a2
vgrashal <vgrashal@gmail.com>
2023-06-24 17:19:03 +0530

    Login Page States Managed

--- src/components/login/LoginBar.jsx
+++ src/components/login/LoginBar.jsx
@@ -1,10 +1,31 @@
 import "./loginbar.css";
+import avatar from "../../../svgs/avatar.svg";
+
+import { useState } from 'react';
 
 function Loginbar(){
+
+    const [ user, setUser]=useState({
+        email:"",
+        password:""
+    })
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
+
     return(
         <>
             <div className="loginbar">
-                <input type="text" id="email"></input> 
+                <img src= {avatar} alt="Avatar"/>
+                <input type="text" placeholder="Enter Email " name="email" value={user.email} onChange={handleChange}></input> 
+                <input type="text" placeholder="Enter Password " name="password" value={user.password} onChange={handleChange}></input> 
+                <button>Login</button>
+                <p1>Don't have an account? <span className="signup">Sign Up</span></p1>
             </div>
         </>
     );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,15 @@
 import "./login.css";
 import Loginbar from "../../components/login/LoginBar";"../../components/login/LoginBar.jsx"
+import graph from "../../../svgs/login_graph.svg"
 
 function Login(){
     return (
     <>  
     <div className="login">
         <div className="leftcontent">
-            
+            <h1>TradeHive</h1>
+            <p>Welcome to TradeHive<br/> Login to access your portfolio and start trading!</p>
+            <img src= {graph} alt="Graph"/>
         </div>
         <div className="rightcontent">
             <Loginbar/>
@@ -17,3 +20,4 @@ function Login(){
 }
 
 export default Login;
+
